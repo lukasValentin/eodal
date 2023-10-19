8c930c639b3031ccc5ddb37fb198e4bcea3921b2
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-10-19 15:28:39 +0200

    updating AWS Earth Search settings

--- eodal/config/stac_providers.py
+++ eodal/config/stac_providers.py
@@ -26,23 +26,25 @@ class STAC_Providers:
     class AWS:
         """Amazon Web Services"""
 
-        URL: str = "https://earth-search.aws.element84.com/v0"
-        S2Level_1C: str = "sentinel-s2-l1c"
-        S2Level_2A: str = "sentinel-s2-l2a"
+        URL: str = "https://earth-search.aws.element84.com/v1"
+        S2Level_1C: str = "sentinel-2-l1c"
+        S2Level_2A: str = "sentinel-2-l2a"
 
         class Sentinel2:
-            product_uri: str = "sentinel:product_id"
-            scene_id: str = "id"
+            product_uri: str = "id"
+            scene_id: str = "s2:product_uri"
             platform: str = "platform"
             tile_id: List[str] = [
-                "sentinel:utm_zone",
-                "sentinel:latitude_band",
-                "sentinel:grid_square",
+                "mgrs:utm_zone",
+                "mgrs:latitude_band",
+                "mgrs:grid_square",
             ]
             sensing_time: str = "datetime"
-            sensing_time_fmt: str = "%Y-%m-%dT%H:%M:%SZ"
+            sensing_time_fmt: str = "%Y-%m-%dT%H:%M:%S.%fZ"
             cloud_cover: str = "eo:cloud_cover"
             epsg: str = "proj:epsg"
+            sun_azimuth_angle = "view:sun_azimuth"
+            sun_zenith_angle = "view:sun_elevation"
 
     class MSPC:
         """Microsoft Planetary Computer"""
@@ -50,7 +52,7 @@ class STAC_Providers:
         URL: str = "https://planetarycomputer.microsoft.com/api/stac/v1"
         S2Level_1C: str = "sentinel-2-l1c"
         S2Level_2A: str = "sentinel-2-l2a"
-        S1RTC: str = "sentinel-1-rtc"  # radiometric and terrain corrected
+        S1RTC: str = "sentinel-1-rtc"  # radiometry and terrain corrected
         S1GRD: str = (
             "sentinel-1-grd"  # corrected to ground range using ellipsoid model WGS84
         )
@@ -66,3 +68,9 @@ class STAC_Providers:
             epsg: str = "proj:epsg"
             sun_zenith_angle = "s2:mean_solar_zenith"
             sun_azimuth_angle = "s2:mean_solar_azimuth"
+
+    class Copernicus:
+        """Copernicus Data Space Ecosystem (not stable yet)"""
+
+        URL: str = "https://catalogue.dataspace.copernicus.eu/stac"
+        S2Level_2A: str = 'SENTINEL-2'
